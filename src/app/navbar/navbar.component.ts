1c3848ec28b07c157d9dbc8613b22b33d80fd21d
Mario <netto.mtx@gmail.com>
2021-02-23 21:56:03 -0300

    ajuste no navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../models/user.model';
 import { Security } from '../utils/security.util';
 import { Router } from '@angular/router';
-
+import { Cart } from '../models/cart.model';
+import { CartUtil } from '../utils/cart.util';
+import { CartItem } from '../models/cart-item.model';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
+  @Input() item: CartItem;
   public user: User;
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.user = Security.getUser();
+
+    console.log('131331313131313');
   }
 
   logout() {
--- src/app/pages/account/store/cart-page/cart-page.component.ts
+++ src/app/pages/account/store/cart-page/cart-page.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../../../../models/cart.model';
+import { CartUtil } from '../../../../utils/cart.util';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
 })
 export class CartPageComponent implements OnInit {
+  public cart: Cart = new Cart();
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadCart();
+  }
+
+  public total() {
+    let total = 0;
+    this.cart.items.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  }
+
+  public loadCart() {
+    this.cart = CartUtil.get();
+  }
+
+  public remove(item) {
+    let index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear() {
+    CartUtil.clear();
+    this.loadCart();
+  }
 }
